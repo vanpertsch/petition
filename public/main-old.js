8d2a5eb82e13bdd5d4dfb6a5ebad41585735ff1e
Vanessa Pertsch <vanessa.pertsch@posteo.de>
2021-11-07 20:33:58 +0100

    fix for getMousePosition on canvas. add layout

---
+++ public/main-old.js
@@ -0,0 +1,27 @@
+(function () {
+
+    console.log("connected");
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var dataURL;
+
+    $("#canvas").mousedown((e) => {
+        ctx.moveTo(e.offsetX, e.offsetY);
+        ctx.beginPath();
+
+        $("#canvas").mousemove((e) => {
+            ctx.lineTo(e.offsetX, e.offsetY);
+            ctx.stroke();
+        });
+
+        $("#canvas").mouseup(() => {
+            dataURL = canvas.toDataURL();
+            const hiddenField = document.getElementById("hiddenfield");
+            hiddenField.value = dataURL;
+            $("#canvas").off("mousemove");
+        });
+    });
+
+
+})();
--- public/main.js
+++ public/main.js
@@ -1,27 +1,49 @@
 (function () {
 
-    console.log("connected");
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("canvas");
+    if (canvas) {
+        const ctx = canvas.getContext('2d');
+        let mouseX, mouseY, mouseDown, dataURL = 0;
 
-    var dataURL;
+        canvas.addEventListener('mousedown', canvas_mouseDown);
+        canvas.addEventListener('mousemove', canvas_mouseMove);
+        window.addEventListener('mouseup', canvas_mouseUp);
 
-    $("#canvas").mousedown((e) => {
-        ctx.moveTo(e.offsetX, e.offsetY);
-        ctx.beginPath();
+        // Get the current mouse position relative to the top-left of the canvas
+        function getMousePos(canvas, e) {
 
-        $("#canvas").mousemove((e) => {
-            ctx.lineTo(e.offsetX, e.offsetY);
-            ctx.stroke();
-        });
+            var rect = canvas.getBoundingClientRect();
 
-        $("#canvas").mouseup(() => {
+            mouseX = (e.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
+            mouseY = (e.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
+
+        }
+
+        // Keep track of the mouse button being pressed and draw a dot at current location
+        function canvas_mouseDown(e) {
+            mouseDown = 1;
+            ctx.moveTo(mouseX, mouseY);
+            ctx.beginPath();
+        }
+
+        // Keep track of the mouse button being released
+        function canvas_mouseUp() {
+            mouseDown = 0;
             dataURL = canvas.toDataURL();
             const hiddenField = document.getElementById("hiddenfield");
             hiddenField.value = dataURL;
-            $("#canvas").off("mousemove");
-        });
-    });
+        }
 
+        function canvas_mouseMove(e) {
+            // Update the mouse co-ordinates when moved
+            getMousePos(canvas, e);
+
+            // Draw a dot if the mouse button is currently being pressed
+            if (mouseDown == 1) {
+                ctx.lineTo(mouseX, mouseY);
+                ctx.stroke();
+            }
+        }
+    }
 
 })();
